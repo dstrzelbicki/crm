72e3d40c815443a1e6c6770b09b7481331e50999
dstrzelbicki <dstrzelbicki@gmail.com>
2025-09-28 17:15:25 +0200

    app added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Welcome to My CRM App</h1>
+        <p>Manage your clients, deals, and tasks with ease.</p>
+        <a href="#get-started" className="btn-primary">Get Started</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
